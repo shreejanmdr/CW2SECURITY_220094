eb7594174f98c6e739a701773e8ba6423bbaad76
shreejanmdr <shreejan0497@gmail.com>
2025-01-30 02:56:58 +0545

    Defining FIXED REQUESTS

--- webApi/backend/routes/userRoutes.js
+++ webApi/backend/routes/userRoutes.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
 const userController = require("../controllers/userControllers");
 
+// POST request to create a user
 router.post("/create", userController.createUser);
+
+// Handle disallowed GET requests
+router.get("/create", (req, res, next) => {
+  res.status(405).json({
+    error: "GET request is not allowed in this Closet website developed by Shreejan Manandhar",
+  });
+});
+
+// Handle disallowed PUT requests
+router.put("/create", (req, res, next) => {
+  res.status(405).json({
+    error: "PUT request is not allowed in this Closet website developed by Shreejan Manandhar",
+  });
+});
+
+// Handle disallowed DELETE requests
+router.delete("/create", (req, res, next) => {
+  res.status(405).json({
+    error: "DELETE request is not allowed in this Closet website developed by Shreejan Manandhar",
+  });
+});
+
 
 router.post("/login", userController.loginUser);
 
