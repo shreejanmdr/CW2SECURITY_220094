1792bee5c03430655aaf479364861ddd5852bd72
shreejanmdr <shreejan0497@gmail.com>
2025-01-29 11:26:17 +0545

    hash token updated

--- webApi/backend/models/userModel.js
+++ webApi/backend/models/userModel.js
@@ -73,14 +73,14 @@ userSchema.pre("save", async function (next) {
     // Generate token
     const resetToken = crypto.randomBytes(20).toString("hex");
   
-    // // Hash token and set to resetPasswordToken field
-    // this.resetPasswordToken = crypto
-    //   .createHash("sha256")
-    //   .update(resetToken)
-    //   .digest("hex");
+    // Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
   
-    // // Set expire
-    // this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+    // Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
   
     return resetToken;
   };
